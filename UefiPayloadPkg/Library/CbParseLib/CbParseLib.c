83312bf747b07a7dc06f4ea86d8416970ba5d90e
Cheng-Chieh Huang <chengchieh@google.com>
2020-12-20 23:44:48 +0900

    Search _SM_ and _SM3_ from F000

--- UefiPayloadPkg/Library/CbParseLib/CbParseLib.c
+++ UefiPayloadPkg/Library/CbParseLib/CbParseLib.c
@@ -16,6 +16,7 @@ required information.
 #include <Library/IoLib.h>
 #include <Library/PcdLib.h>
 #include <Uefi/UefiBaseType.h>
+#include <IndustryStandard/SmBios.h>
 
 /**
   Convert a packed value from cbuint64 to a UINT64 value.
@@ -442,17 +443,38 @@ ParseMemoryInfo(IN BL_MEM_INFO_CALLBACK MemInfoCallback, IN VOID *Params) {
 }
 
  // Find _SM_ in memory in F0000
-UINTN FindRsdpPtrInLowMem(){
+UINTN FindSMBIOSPtrInLowMem(UINT32* Size){
   UINTN base;
-  const UINT64 RsdpTag = 0x2052545020445352;
   for (base = 0xF0000; base < 0x100000; base++) {
-    if (*(UINT64*)base == RsdpTag) {
-      DEBUG((DEBUG_INFO, "Found RSDP ptr in: 0x%08x\n", base));
+    SMBIOS_TABLE_ENTRY_POINT* smbios = (SMBIOS_TABLE_ENTRY_POINT*)base;
+    if (smbios->AnchorString[0] == 0x5f &&
+        smbios->AnchorString[1] == 0x53 &&
+        smbios->AnchorString[2] == 0x4d &&
+        smbios->AnchorString[3] == 0x5f &&
+        smbios->IntermediateAnchorString[0] == 0x5f &&
+        smbios->IntermediateAnchorString[1] == 0x44 &&
+        smbios->IntermediateAnchorString[2] == 0x4d &&
+        smbios->IntermediateAnchorString[3] == 0x49 &&
+        smbios->IntermediateAnchorString[4] == 0x5f) {
+      *Size = 0x1f;
+      DEBUG((DEBUG_INFO, "Found SMBIOS anchor ptr in: 0x%08x\n", base));
+      return base;
+    }
+  }
+  for (base = 0xF0000; base < 0x100000; base++) {
+    SMBIOS_TABLE_3_0_ENTRY_POINT* smbios = (SMBIOS_TABLE_3_0_ENTRY_POINT*)base;
+    if (smbios->AnchorString[0] == 0x5f &&
+        smbios->AnchorString[1] == 0x53 &&
+        smbios->AnchorString[2] == 0x4d &&
+        smbios->AnchorString[3] == 0x33 &&
+        smbios->AnchorString[4] == 0x5f) {
+      *Size = 0x18;
+      DEBUG((DEBUG_INFO, "Found SMBIOS3 anchor ptr in: 0x%08x\n", base));
       return base;
     }
   }
   DEBUG((DEBUG_INFO, "SMBIOS header is not found in F0000\n"));
-  return 0xf5b20;
+  return 0xf5b40;
 }
 
  // " RSD PTR" in hex, 8 bytes.
@@ -490,14 +512,16 @@ ParseSystemTable(OUT SYSTEM_TABLE_INFO *SystemTableInfo) {
   EFI_STATUS Status;
   VOID *MemTable;
   UINT32 MemTableSize;
+  UINT32 SMBIOSHdrSize;
   UINTN RsdpPtr;
 
   RsdpPtr = FindRsdpPtrInLowMem();
 
   SystemTableInfo->AcpiTableBase = RsdpPtr;
   SystemTableInfo->AcpiTableSize = 14;
-  SystemTableInfo->SmbiosTableBase = 0xf5b40;
-  SystemTableInfo->SmbiosTableSize = 0x1F;
+  
+  SystemTableInfo->SmbiosTableBase = FindSMBIOSPtrInLowMem(&SMBIOSHdrSize);
+  SystemTableInfo->SmbiosTableSize = SMBIOSHdrSize;
 
   return RETURN_SUCCESS;
 

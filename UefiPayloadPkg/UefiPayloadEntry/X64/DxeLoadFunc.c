00796b3df5a3f64f39563f3642a6e956f5b344d3
Cheng-Chieh Huang <chengchieh@google.com>
2020-12-30 13:54:39 +0000

    Avoid panic in debug mode.

--- UefiPayloadPkg/UefiPayloadEntry/X64/DxeLoadFunc.c
+++ UefiPayloadPkg/UefiPayloadEntry/X64/DxeLoadFunc.c
@@ -66,8 +66,8 @@ HandOffToDxeCore (
   //
   // Get the address and size of the GHCB pages
   //
-  GhcbBase = (VOID *) PcdGet64 (PcdGhcbBase);
-  GhcbSize = PcdGet64 (PcdGhcbSize);
+  // GhcbBase = (VOID *) PcdGet64 (PcdGhcbBase);
+  // GhcbSize = PcdGet64 (PcdGhcbSize);
 
   PageTables = 0;
   if (FeaturePcdGet (PcdDxeIplBuildPageTables)) {
